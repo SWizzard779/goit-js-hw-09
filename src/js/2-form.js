49179e7b217e1b1866627ec62a7d6401dabd937b
SWizzard779 <sofia.nesimko@gmail.com>
2024-03-08 12:33:46 +0100

    form

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+const textInput = form.elements.message;
+const emailInput = form.elements.email;
+
+textInput.value = localStorage.getItem(localStorageKey) ?? '';
+emailInput.value = localStorage.getItem(localStorageKey) ?? '';
+
+form.addEventListener('input', event => {
+  localStorage.setItem(localStorageKey, event.target.value);
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault;
+
+  if (validateForm()) {
+    form.submit;
+  } else {
+    alert('Please fill in all the form fields in order to submit it!');
+  }
+
+  console.log({
+    message: event.target.elements.message.value,
+    email: event.target.elements.email.value,
+  });
+
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
+
+function validateForm() {
+  return textInput.value.trim() !== '' && emailInput.value.trim() !== '';
+}
